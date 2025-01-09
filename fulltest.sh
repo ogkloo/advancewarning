bd2447c81b497d18b2864c3110a87319e0ac7c8f
Sam Shippey <sshippey@pdx.edu>
2025-01-09 19:50:28 +0000

    Change which tests to run.

--- fulltest.sh
+++ fulltest.sh
@@ -2,13 +2,18 @@
 sudo killall quictun-server 2> /dev/null
 sudo killall quictun-client 2> /dev/null
 sudo killall http-server 2> /dev/null
+sudo killall iplay
 
 # Build istream player
 ./istream-player/build.sh 1> /dev/null
 
 # It should clean up after itself anyway but this is worth the 5 seconds it takes
 sudo -E env PATH=$PATH mn --clean > /dev/null 2> /dev/null
-sudo -E env PATH=$PATH python netemtests.py --video videos/academic/multi_resolution.mpd --results $1
+sudo -E env PATH=$PATH python netemtests.py --video videos/siyuan/multi_resolution.mpd \
+videos/lanqiuchang/multi_resolution.mpd \
+--results $1
+#videos/academic/multi_resolution.mpd \
+#videos/runner/multi_resolution.mpd \
 
 #mn --clean > /dev/null 2> /dev/null
 #python3 netemtests.py --video videos/siyuan/multi_resolution.mpd --results results/siyuan
@@ -20,3 +25,4 @@ sudo -E env PATH=$PATH python netemtests.py --video videos/academic/multi_resolu
 sudo killall quictun-server 2> /dev/null
 sudo killall quictun-client 2> /dev/null
 sudo killall http-server 2> /dev/null
+sudo killall iplay
