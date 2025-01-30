fa587b7abbb0d0f096bc1852234a1c62c57a0a9b
Sam <weathercontrol@tuta.io>
2025-01-30 11:23:03 -0800

    Make less verbose

--- run_mininet_script.sh
+++ run_mininet_script.sh
@@ -1,3 +1,4 @@
 # Necessary Because otherwise you get problems with permissions and these commands are annoying to type
 
-sudo -E env PATH=$PATH python $1 "${@:2}"
+#sudo -E env PATH=$PATH python $1 "${@:2}"
+sudo -E python $1 "${@:2}"
